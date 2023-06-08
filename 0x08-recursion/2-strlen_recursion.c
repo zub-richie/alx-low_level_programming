efdcedc3511c3e7590c24f60d7f71e910420a317
zub-richie <zub.richie@gmail.com>
2023-06-08 15:51:32 -0700

    Task 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen_recursion - This function count the number of string
+ *
+ * @s : The string that'll be counted 
+ *
+ * Return: Print the total number of the string.
+ */
+
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return 0;
+	}
+	else
+	{
+		return 1 + _strlen_recursion(s + 1);
+	}
+
+}
