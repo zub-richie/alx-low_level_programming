9b5d02e8a8dba09a8df5175e2820f6475259b350
zub-richie <zub.richie@gmail.com>
2023-06-09 19:04:29 -0700

    Output the number of arrays

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *  main - Outputs the name of the program
+ *
+ *   @argc - The number of command line arguments
+ *    @argv - An array containing the program command line arguments
+ *
+ *     Return: Always return 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
