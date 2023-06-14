5912f1304a14de9c03fc12bff9f4596c982c7890
zub-richie <zub.richie@gmail.com>
2023-06-14 15:40:09 -0700

    A C program that prints using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is the function
+ *
+ * Return: 0 if successful.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
