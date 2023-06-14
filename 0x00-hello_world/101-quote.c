dfff9a3a7d82b94cc6de369edd0cb8692b15b1c7
zub-richie <zub.richie@gmail.com>
2023-06-14 16:59:51 -0700

    Usine putchar to print characters

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ *
+ * Return: 1 if successful.
+ */
+
+int main(void)
+{
+	const char *c = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		putchar(c[i]);
+		i++;
+	}
+
+	return (1);
+}
