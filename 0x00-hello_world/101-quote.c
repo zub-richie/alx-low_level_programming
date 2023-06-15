36e3c18f47413f64a59e09c03cd841fe3715eab2
zub-richie <zub.richie@gmail.com>
2023-06-14 17:04:50 -0700

    Usine putchar to print characters

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	const char *c = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *c = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	int i = 0;
 
 	while (c[i] != '\0')
